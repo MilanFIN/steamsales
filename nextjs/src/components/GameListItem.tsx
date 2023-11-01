53d25779f290dbc27a4cfe541f609fee384459c0
milanfin <milan.makipaa@gmail.com>
2023-11-01 21:23:55 +0200

    minor bugfix

--- nextjs/src/components/GameListItem.tsx
+++ nextjs/src/components/GameListItem.tsx
@@ -39,7 +39,7 @@ function GameListItem(props: GameListItemProps) {
     }
   }, [props.active]);
 
-  const numberWithSpaces = function (x) {
+  const numberWithSpaces = function (x: number) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
   };
 
